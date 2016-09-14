15bb3326b5c0071e3f296ad5436b7e16772dff47
david <david.vela.tirado@gmail.com>
2016-09-14 20:26:09 +0200

    Add project files.

---
+++ FirstWordAddIn/Ribbon1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FirstWordAddIn
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabOthers = this.Factory.CreateRibbonTab();
+            this.groupSaveAs = this.Factory.CreateRibbonGroup();
+            this.buttonPDF = this.Factory.CreateRibbonButton();
+            this.buttonXps = this.Factory.CreateRibbonButton();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.toggleButtonXMLDesc = this.Factory.CreateRibbonToggleButton();
+            this.toggleButton_keepText = this.Factory.CreateRibbonToggleButton();
+            this.buttonXMLDesc_All = this.Factory.CreateRibbonButton();
+            this.button_InsertXMLD = this.Factory.CreateRibbonButton();
+            this.button_cleanup_xmlD = this.Factory.CreateRibbonButton();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.label1 = this.Factory.CreateRibbonLabel();
+            this.tabOthers.SuspendLayout();
+            this.groupSaveAs.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabOthers
+            // 
+            this.tabOthers.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabOthers.Groups.Add(this.groupSaveAs);
+            this.tabOthers.Groups.Add(this.group1);
+            this.tabOthers.Groups.Add(this.group2);
+            this.tabOthers.Label = "Others Add_Ins";
+            this.tabOthers.Name = "tabOthers";
+            // 
+            // groupSaveAs
+            // 
+            this.groupSaveAs.Items.Add(this.buttonPDF);
+            this.groupSaveAs.Items.Add(this.buttonXps);
+            this.groupSaveAs.Label = "Save As";
+            this.groupSaveAs.Name = "groupSaveAs";
+            // 
+            // buttonPDF
+            // 
+            this.buttonPDF.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.buttonPDF.Image = global::FirstWordAddIn.Properties.Resources.pdf_icon_48x48;
+            this.buttonPDF.Label = "PDF";
+            this.buttonPDF.Name = "buttonPDF";
+            this.buttonPDF.ShowImage = true;
+            this.buttonPDF.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // buttonXps
+            // 
+            this.buttonXps.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.buttonXps.Image = global::FirstWordAddIn.Properties.Resources.icon_xps;
+            this.buttonXps.Label = "XPS";
+            this.buttonXps.Name = "buttonXps";
+            this.buttonXps.ShowImage = true;
+            this.buttonXps.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonXps_Click);
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.toggleButtonXMLDesc);
+            this.group1.Items.Add(this.toggleButton_keepText);
+            this.group1.Items.Add(this.buttonXMLDesc_All);
+            this.group1.Items.Add(this.button_InsertXMLD);
+            this.group1.Items.Add(this.button_cleanup_xmlD);
+            this.group1.Label = "EasyQ";
+            this.group1.Name = "group1";
+            // 
+            // toggleButtonXMLDesc
+            // 
+            this.toggleButtonXMLDesc.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.toggleButtonXMLDesc.Image = global::FirstWordAddIn.Properties.Resources.xmlIcon;
+            this.toggleButtonXMLDesc.Label = "AUTO XML Desription";
+            this.toggleButtonXMLDesc.Name = "toggleButtonXMLDesc";
+            this.toggleButtonXMLDesc.ShowImage = true;
+            this.toggleButtonXMLDesc.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.toggleButtonXMLDesc_Click);
+            // 
+            // toggleButton_keepText
+            // 
+            this.toggleButton_keepText.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.toggleButton_keepText.Image = global::FirstWordAddIn.Properties.Resources.keeptext;
+            this.toggleButton_keepText.Label = "Keep Previous XML Description";
+            this.toggleButton_keepText.Name = "toggleButton_keepText";
+            this.toggleButton_keepText.ShowImage = true;
+            this.toggleButton_keepText.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.toggleButton_keepText_Click);
+            // 
+            // buttonXMLDesc_All
+            // 
+            this.buttonXMLDesc_All.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.buttonXMLDesc_All.Image = global::FirstWordAddIn.Properties.Resources.xml_128;
+            this.buttonXMLDesc_All.Label = "Assign ALL XML Descriptions";
+            this.buttonXMLDesc_All.Name = "buttonXMLDesc_All";
+            this.buttonXMLDesc_All.ShowImage = true;
+            this.buttonXMLDesc_All.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_XMLDesc_All);
+            // 
+            // button_InsertXMLD
+            // 
+            this.button_InsertXMLD.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button_InsertXMLD.Image = global::FirstWordAddIn.Properties.Resources.download;
+            this.button_InsertXMLD.Label = "Select to Insert XML Descriptions ";
+            this.button_InsertXMLD.Name = "button_InsertXMLD";
+            this.button_InsertXMLD.ShowImage = true;
+            this.button_InsertXMLD.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button_InsertXMLD_Click);
+            // 
+            // button_cleanup_xmlD
+            // 
+            this.button_cleanup_xmlD.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button_cleanup_xmlD.Image = global::FirstWordAddIn.Properties.Resources.images;
+            this.button_cleanup_xmlD.Label = "Clean Up ALL XML Descriptions";
+            this.button_cleanup_xmlD.Name = "button_cleanup_xmlD";
+            this.button_cleanup_xmlD.ShowImage = true;
+            this.button_cleanup_xmlD.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button_cleanup_xmlD_Click);
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.label1);
+            this.group2.Label = "Information";
+            this.group2.Name = "group2";
+            // 
+            // label1
+            // 
+            this.label1.Label = "Thanks for using this Add";
+            this.label1.Name = "label1";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tabOthers);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tabOthers.ResumeLayout(false);
+            this.tabOthers.PerformLayout();
+            this.groupSaveAs.ResumeLayout(false);
+            this.groupSaveAs.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabOthers;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupSaveAs;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonPDF;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton toggleButtonXMLDesc;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonXps;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonXMLDesc_All;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button_cleanup_xmlD;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button_InsertXMLD;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton toggleButton_keepText;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ FirstWordAddIn/Ribbon1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using System.IO;
+using word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Word;
+
+
+namespace FirstWordAddIn
+{
+    public partial class Ribbon1
+    {
+        private bool bKeepPrevText = false;
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "QuickExport.pdf";
+
+            Globals.ThisAddIn.Application.ActiveDocument.ExportAsFixedFormat(
+                Path.Combine(desktopFolder, fileName),
+                word.WdExportFormat.wdExportFormatPDF,
+                OpenAfterExport: true);
+        }
+
+        private void buttonXps_Click(object sender, RibbonControlEventArgs e)
+        {
+            string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "QuickExport.xps";
+
+            Globals.ThisAddIn.Application.ActiveDocument.ExportAsFixedFormat(
+                Path.Combine(desktopFolder, fileName),
+                word.WdExportFormat.wdExportFormatXPS,
+                OpenAfterExport: true);
+        }
+
+        //******************************    xml BUTTONS   ************************************************//
+
+        /****************************************************************************************
+        *        AUTO ASSIGN  
+        *****************************************************************************************/
+        private void toggleButtonXMLDesc_Click(object sender, RibbonControlEventArgs e)
+        {
+
+            // if active -> embeed the xml name automatically
+            Document myDoc = Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveDocument);
+
+
+
+            //if (this.toggleButtonXMLDesc.Checked == true )
+            //Globals.ThisAddIn.Application.XMLSelectionChange += Application_XMLSelectionChange;
+            //else Globals.ThisAddIn.Application.XMLSelectionChange -= Application_XMLSelectionChange;
+
+            //Globals.ThisAddIn.Application.
+            // myDoc.ContentControlAfterAdd += MyDoc_ContentControlAfterAdd;
+            // += new contentcontrolafteradd
+            //myDoc.ContentControlAfterAdd += new ContentControlAddedEventHandler(InsertXMLProperties);
+
+        }
+
+
+        private void MyDoc_ContentControlAfterAdd(word.ContentControl NewContentControl, bool InUndoRedo)
+        {
+            //xPath is empty !!!!!!!!
+            String xPath = this.getXMLDesc(NewContentControl.XMLMapping.PrefixMappings, NewContentControl.XMLMapping.XPath);
+            NewContentControl.Tag = xPath;
+            NewContentControl.Title = xPath;
+            //throw new NotImplementedException();
+        }
+
+
+        /****************************************************************************************
+        *        ASSIGN ALL  
+        *****************************************************************************************/
+        private void button1_XMLDesc_All(object sender, RibbonControlEventArgs e)
+        {
+            Document myDoc = Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveDocument);
+            //Microsoft.Office.Interop.Word.ContentControl cc = this.Application.ActiveDocument.ContentControls[1];
+            /*Microsoft.Office.Interop.Word.ContentControl cc = myDoc.ContentControls[1];
+                String xPath = cc.XMLMapping.XPath;
+                cc.Tag = xPath.Substring(27);
+                cc.Title = xPath.Substring(27);
+                cc.Range.Text = "I can run"; */
+
+            // loop over all the xml parts and assign the name. 
+            //ContentControls ContentControls { get; }
+            //Microsoft.Office.Interop.Word.ContentControls ContentControls { get; }
+
+            foreach (word.Section wordSection in myDoc.Sections)
+            {
+                var footer = wordSection.Footers[word.WdHeaderFooterIndex.wdHeaderFooterPrimary];
+                var header = wordSection.Headers[word.WdHeaderFooterIndex.wdHeaderFooterPrimary];
+                this.loopContentControls(footer.Range.ContentControls, this.bKeepPrevText);
+                this.loopContentControls(header.Range.ContentControls, this.bKeepPrevText);
+
+                /*word.Range footerRange = footer.Range;
+                footerRange.Font.ColorIndex = word.WdColorIndex.wdDarkRed;
+                footerRange.Font.Size = 20;
+                footerRange.Text = "Confidential";*/
+
+            }
+
+            foreach (Microsoft.Office.Interop.Word.ContentControl cc in myDoc.ContentControls)
+            {
+                //String xPath = cc.XMLMapping.XPath;
+                /*try
+                {
+                    cc.Tag = xPath.Substring(27);
+                    cc.Title = xPath.Substring(27);
+                }
+                catch (System.IndexOutOfRangeException ex)
+                {
+                    //System.Console.WriteLine(e.Message);
+                    // Set IndexOutOfRangeException to the new exception's InnerException.
+                    //throw new System.ArgumentOutOfRangeException("index parameter is out of range.", e);
+                }
+                catch (System.ArgumentOutOfRangeException ex) {
+                    cc.Tag = xPath;
+                    cc.Title = xPath;
+                }*/
+
+                String xPath = this.getXMLDesc(cc.XMLMapping.PrefixMappings, cc.XMLMapping.XPath);
+
+                if (this.bKeepPrevText == true) {
+                    if (cc.Tag == "" || cc.Tag == null )
+                        cc.Tag = xPath;
+            
+                    if ( cc.Title == "" ||  cc.Title == null)
+                        cc.Title = xPath;
+                    
+                } else   {
+                    cc.Tag = xPath;
+                    cc.Title = xPath;
+                    //cc.Range.Text = "I can run";
+                }
+
+            }
+        }
+
+        /****************************************************************************************
+        *        SELECT TO INSERT 
+        *****************************************************************************************/
+        private void button_InsertXMLD_Click(object sender, RibbonControlEventArgs e)
+        {
+            Microsoft.Office.Interop.Word.Selection wordSelection = Globals.ThisAddIn.Application.Selection;
+            
+            foreach (Microsoft.Office.Interop.Word.ContentControl cc in wordSelection.ContentControls)
+            {
+                String xPath = this.getXMLDesc(cc.XMLMapping.PrefixMappings, cc.XMLMapping.XPath);
+                cc.Tag = xPath;
+                cc.Title = xPath;
+                //cc.Range.Text = "I can run";
+            }
+        }
+
+        /****************************************************************************************
+        *        TOGGLE KEEP TEXT 
+        *****************************************************************************************/
+        private void toggleButton_keepText_Click(object sender, RibbonControlEventArgs e)
+        {
+            RibbonToggleButton target = sender as RibbonToggleButton;
+            this.bKeepPrevText = target.Checked;
+     
+        }
+
+        /****************************************************************************************
+        *        CLEAN UP ALL  
+        *****************************************************************************************/
+        private void button_cleanup_xmlD_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document myDoc = Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveDocument);
+            foreach (Microsoft.Office.Interop.Word.ContentControl cc in myDoc.ContentControls)
+            {
+                cc.Tag = "";
+                cc.Title = "";
+            }
+            // Document myHeader = Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application); 
+        }
+
+        //******************************    FUNCTIONS   ************************************************// 
+
+        /****************************************************************************************
+        *        GET DESCRIPTION 
+        *****************************************************************************************/
+        private string getXMLDesc(string sNamespace, string sPath)
+        {
+            string xmlDesc, slNamespace, slPath;
+            slNamespace = sNamespace;
+            slPath = sPath;
+
+            if (slNamespace != "" && slPath != null)
+            {
+                slNamespace = slNamespace.Replace("/ns0:", " ");
+                slNamespace = slNamespace.Replace("xmlns:ns0=", " ");
+                slNamespace = slNamespace.Replace(":root[1]", " ");
+            }
+            else return xmlDesc = "";
+
+            if (slPath != "" && slPath != null)
+            {
+                slPath = slPath.Replace("/ns0:", "");
+                slPath = slPath.Replace("/ns1:", "");
+                slPath = slPath.Replace("root[1]", "");
+            }
+            else return xmlDesc = "";
+
+            // if (slNamespace.Length > 20)           xmlDesc = slPath;
+            // else  xmlDesc =  slPath + slNamespace; 
+
+            xmlDesc = slPath + slNamespace;
+            return xmlDesc;
+
+        }
+
+        /****************************************************************************************
+        *        LOOP ALL CONTENT CONTROLS   
+        *****************************************************************************************/
+        private void loopContentControls(word.ContentControls aContentControls, bool sKeepPrevText)
+        {
+
+            foreach (Microsoft.Office.Interop.Word.ContentControl cc in aContentControls)
+            {
+                String xPath = this.getXMLDesc(cc.XMLMapping.PrefixMappings, cc.XMLMapping.XPath);
+
+                if (sKeepPrevText == true)
+                {
+                    if (cc.Tag == "" || cc.Tag == null) cc.Tag = xPath;
+                    if (cc.Title == "" || cc.Title == null) cc.Title = xPath;
+                }
+                else
+                {
+                    cc.Tag = xPath;
+                    cc.Title = xPath;
+                }
+
+            }
+        }
+
+
+    }
+}
+           
